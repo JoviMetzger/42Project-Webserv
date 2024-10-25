2ebf987664012398dd50128e73f3076b4b2efccd
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2024-10-25 20:11:08 +0200

    Update Location.hpp

--- includes/Location.hpp
+++ includes/Location.hpp
@@ -11,33 +11,33 @@
 class Location
 {
 	private:
-		std::string					_path; // This relates to the part of the url thats being requested >> location "/" {}
-		std::string					_root; // Local pathing to what directory should be served
-		std::string					_index; // what specific file should be served
+		std::string			_path; 			// This relates to the part of the url thats being requested >> location "/" {}
+		std::string			_root; 			// Local pathing to what directory should be served
+		std::string			_index; 		// what specific file should be served
 		std::vector<std::string>	_allowedMethods;
-		bool						_autoindex;
-		std::string					_redir; // Redirect
-		int							_redirStatusCode;  // Status code for the redirect
+		bool				_autoindex;
+		std::string			_redir; 		// Redirect
+		int				_redirStatusCode; 	// Status code for the redirect
 
 	public:
-		Location();		// Constructor
+		Location();	// Constructor
 		~Location() {}	// Destructor
 
 		// Getter
-		bool						getAutoindex() const;
-		std::string					getPath() const;
-		std::string 				getRoot() const;
-		std::string 				getIndex() const;
-		std::string					getRedir() const;
+		bool				getAutoindex() const;
+		std::string			getPath() const;
+		std::string 			getRoot() const;
+		std::string 			getIndex() const;
+		std::string			getRedir() const;
 		std::vector<std::string>	getAllowedMethods() const;
-		int							getRedirStatusCode() const;
+		int				getRedirStatusCode() const;
 
 		// Setter
-		void						setPath(const std::string &path);
-		void						setRoot(const std::string &root);
-		void						setIndex(const std::string &index);
-		void						setAllowedMethods(const std::vector<std::string> &methods);
-		void						setAutoindex(bool autoindex);
-		void						setRedir(const std::string &redir);
-		void						setRedirStatusCode(const int &statusCode);
+		void				setPath(const std::string &path);
+		void				setRoot(const std::string &root);
+		void				setIndex(const std::string &index);
+		void				setAllowedMethods(const std::vector<std::string> &methods);
+		void				setAutoindex(bool autoindex);
+		void				setRedir(const std::string &redir);
+		void				setRedirStatusCode(const int &statusCode);
 };
